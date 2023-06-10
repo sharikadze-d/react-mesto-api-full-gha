c8f53a357e58699493f129bf8fbfadcd379ccb6f
Denis Sharikadze <denissharikadze94@yandex.ru>
2023-06-10 15:04:34 +0400

    replace app.use(cors());

--- backend/app.js
+++ backend/app.js
@@ -13,12 +13,12 @@ const {
   loginValidation,
 } = require('./middlewares/validation');
 
-const app = express();
 const PORT = 3001;
+const app = express();
+app.use(cors());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
