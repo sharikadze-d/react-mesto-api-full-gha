c913462bcb02668c3875b5d7d9e03f967180146b
Denis Sharikadze <denissharikadze94@yandex.ru>
2023-06-11 18:29:10 +0400

    feat: add .env

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = 'supersupersecret';
+const JWT_SECRET = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret';
 
 const getJwtToken = (id) => jwt.sign({ _id: id }, JWT_SECRET, { expiresIn: '7d' });
 
